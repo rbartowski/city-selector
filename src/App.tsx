212f0c7313b88de50ffb55fca538e27142dc9b43
Ramiro Rodriguez <ramiro@Ramiros-MBP.fibertel.com.ar>
2020-10-23 17:25:32 -0300

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import CitySelector from './components/CitySelector/CitySelector';
+
+function App() {
+  return (
+    <div className="App">
+      <CitySelector></CitySelector>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CitySelector/CitySelector.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './CitySelector.css';
+
+function CitySelector() {
+  return (
+    <div className="CitySelector">
+    </div>
+  );
+}
+
+export default CitySelector;
